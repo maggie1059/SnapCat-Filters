ffa1cc995617c9895bbb4627d45d221c31a6c43f
maggie1059 <34173352+maggie1059@users.noreply.github.com>
2020-04-21 20:14:14 -0700

    main

--- main.py
+++ main.py
@@ -11,28 +11,16 @@ from model import Model
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def main():
-    data_dir = os.path.dirname(__file__) + './cat-dataset/CAT_00'
-    image_paths, coordinate_paths = get_cats(data_dir)
+    img_path = os.path.dirname(__file__) + './processed_imgs/img.npy'
+    coord_path = os.path.dirname(__file__) + './processed_coords/coord.npy'
+    images = np.load(img_path)
+    coords = np.load(coord_path)
     model = Model(224, 224)
     model.compile_model()
-    images, coords = load_data(image_paths, coordinate_paths)
     images = np.expand_dims(images, axis=-1)
     coords = np.reshape(coords, (-1,18,))
     model.train_model(images, coords)
 
-def get_cats(search_path):
-    image_paths = []
-    coordinate_paths= []
-    for subdir, dirs, files in os.walk(search_path):
-        for filename in files:
-            filepath = subdir + os.sep + filename
-            if filepath.endswith(".jpg"):
-                image_paths.append(filepath)
-            if filepath.endswith(".jpg.cat"):
-                coordinate_paths.append(filepath)
-    image_paths = sorted(image_paths)
-    coordinate_paths = sorted(coordinate_paths)
-    return image_paths, coordinate_paths
 
 def load_data(image_paths, coordinate_paths):
     images = []
