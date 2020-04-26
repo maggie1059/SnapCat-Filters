6e352f8c530acd38e96b8fedf52111f6e2829137
maggie1059 <34173352+maggie1059@users.noreply.github.com>
2020-04-26 15:10:38 -0700

    import

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Input
 from tensorflow.keras import Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
