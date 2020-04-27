57bbd7c8cf54595ce50d6c8c8cc9d25bd2cb6049
Soryan Kumar <soryan_kumar@brown.edu>
2020-04-26 20:55:00 -0400

    James in progress
    working implementation of James filter and fixes to ashhat filter

--- add_filter.py
+++ add_filter.py
@@ -29,6 +29,10 @@ def ashhat_filter(image, coord):
 
     hat = cv2.imread('filters/ashhat.png',cv2.IMREAD_UNCHANGED)
 
+    #mirror ashhat if left point is higher than right
+    if left_tip[0] > right_tip[0]:
+        hat = cv2.flip(hat,1)
+
     #set hat angle using tips only if out of bounds
     #set width accordingly
     if left_tip[1] <0 or left_tip[0] <0:
@@ -56,6 +60,10 @@ def ashhat_filter(image, coord):
     image = np.array(image,dtype =np.float64)
     image /= 255.0
 
+    new_image_chan = np.copy(image[:,:,0])
+    image[:,:,0] = image[:,:,2]
+    image[:,:,2] = new_image_chan
+
     for i in range(0,hw):       # Overlay the filter based on the alpha channel
         for j in range(0,hh):
             #use ear tips if out of bounds
@@ -66,6 +74,91 @@ def ashhat_filter(image, coord):
             else:
                 if hat[i,j,-1] != 0 and i+left_out_ear[1]-hw >=0 and j+left_out_ear[0] >=0:
                     image[i+left_out_ear[1]-hw,j+left_out_ear[0],:] = hat[i,j,:-1]
+
+    plt.imshow(image)
+    plt.show()
+
+
+def james_filter(image,coord):
+
+    #remove first element (in the care of size 19)
+    coord = coord[1:]
+    #reshape if necessary
+    coord = coord.reshape(9,2)
+    #use tips when points are out of bounds
+    left_tip = coord[4,:]
+    right_tip = coord[-2,:]
+
+    #otherwise use base of ear
+    left_out_ear = coord[3,:]
+    right_out_ear = coord[-1,:]
+
+    #try using mouth point for face
+    mouth_point = coord[2,:]
+
+
+    james = cv2.imread('filters/james.png',cv2.IMREAD_UNCHANGED)
+
+    #mirror james if left point is higher than right
+    if left_out_ear[0] > right_out_ear[0]:
+        james = cv2.flip(james,1)
+
+    #set hat angle using tips only if out of bounds
+    #set width accordingly
+    '''
+    if left_tip[1] <0 or left_tip[0] <0:
+        angle = np.arctan((left_tip[1]-right_tip[1])/(left_tip[0]-right_tip[0]))
+        hat_width = np.linalg.norm(right_tip - left_tip)
+    else:
+        angle = np.arctan((left_out_ear[1]-right_out_ear[1])/(left_out_ear[0]-right_out_ear[0]))
+        hat_width = np.linalg.norm(right_out_ear - left_out_ear)
+    '''
+    angle = np.arctan((left_tip[1]-right_tip[1])/(left_tip[0]-right_tip[0]))
+    james_width = np.linalg.norm(right_tip - left_tip)
+    james_height = mouth_point[1] - min(right_tip[1],left_tip[1])
+    #print("left_tip", left_tip)
+    #print("right_tip", right_tip)
+    #print("mouth_point", mouth_point)
+
+    #print("height", james_height)
+    #print("width",james_width)
+
+    rotate_james = imutils.rotate_bound(james,angle*180/np.pi)
+
+    old_height, old_width, channel = james.shape
+
+    #hat_height = old_height*hat_width/old_width
+    james = cv2.resize(rotate_james, (int(1.3*james_width),int(1.3*james_height)))
+    hw,hh,hc = james.shape
+
+    james = np.array(james,dtype = np.float64)
+    james /= 255.0
+
+    new_chan = np.copy(james[:,:,0])
+    james[:,:,0] = james[:,:,2]
+    james[:,:,2] = new_chan
+
+    image = np.array(image,dtype =np.float64)
+    image /= 255.0
+
+    new_image_chan = np.copy(image[:,:,0])
+    image[:,:,0] = image[:,:,2]
+    image[:,:,2] = new_image_chan
+
+    for i in range(0,hw):       # Overlay the filter based on the alpha channel
+        for j in range(0,hh):
+            if james[i,j,-1] != 0 and i+left_tip[1] >=0 and j+left_tip[0] >=0:
+                image[i+left_tip[1],j+left_tip[0],:] = james[i,j,:-1]
+            '''
+            #use ear tips if out of bounds
+            if left_tip[1] <0 or left_tip[0] <0:
+                if hat[i,j,-1] != 0 and i+left_tip[1] >=0 and j+left_tip[0] >=0:
+                    image[i+left_tip[1],j+left_tip[0],:] = hat[i,j,:-1]
+            #use ear base otherwise
+            else:
+                if hat[i,j,-1] != 0 and i+left_out_ear[1]-hw >=0 and j+left_out_ear[0] >=0:
+                    image[i+left_out_ear[1],j+left_out_ear[0],:] = hat[i,j,:-1]
+            '''
 
     plt.imshow(image)
     plt.show()
@@ -89,6 +182,16 @@ for ind in range(len(images)):
     coord = coords[ind]
 
     ashhat_filter(image,coord)
+    james_filter(image,coord)
+    '''
+    coord = coord[1:]
+    coord = coord.reshape(9,2)
+    plt.imshow(image)
+    plt.scatter(coord[2,0],coord[2,1], c='k')
+    #plt.scatter(coord[-2,0],coord[-2,1], c='k')
+    plt.show()
+    '''
+
 
 '''
     coord = coord[1:]
