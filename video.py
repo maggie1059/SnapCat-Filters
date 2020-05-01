ddf7e35ffb7db046be36dfaa931bb8dbcef49bb6
Soryan Kumar <soryan_kumar@brown.edu>
2020-05-01 16:01:19 -0400

    video.py and kitkat live

--- video.py
+++ video.py
@@ -2,7 +2,7 @@ import argparse
 import cv2 as cv
 from cv2 import imread
 import numpy as np
-from skimage.color import rgb2gray
+from skimage.color import rgb2gray, gray2rgb
 from skimage.transform import resize
 from model import Model
 import tensorflow as tf
@@ -11,9 +11,9 @@ from add_filter import ashhat_filter
 def preprocess(image):
     # Change to gray scale
     image = rgb2gray(image)
-    
+
     # Pad to square (pad bottom and right side only)
-    max_dim = np.max(image.shape) 
+    max_dim = np.max(image.shape)
     new_image = np.zeros((max_dim, max_dim))
     new_image[0:image.shape[0], 0:image.shape[1]] = image
 
@@ -22,7 +22,7 @@ def preprocess(image):
     new_image = gray2rgb(new_image) / 255.
 
     scaling_factor = max_dim / 224
-    
+
     return np.reshape(new_image, (1, 224, 224, 3)), scaling_factor
 
 
@@ -33,7 +33,8 @@ def main():
     model.compile_model()
     model.load_trained_model()
 
-    cap = cv.VideoCapture('live.mp4')
+    #cap = cv.VideoCapture('live.mp4')
+    cap = cv.VideoCapture(0)
 
     while(cap.isOpened()):
         ret, frame = cap.read()
@@ -45,8 +46,11 @@ def main():
 
         coords = np.floor(predicted * scaling_factor).astype(np.int)
         new_im = ashhat_filter(frame, coords)
-        new_im *= 255
-        new_im = new_im.astype(np.int)
+        new_im_chan = np.copy(new_im[:,:,0])
+        new_im[:,:,0] = new_im[:,:,2]
+        new_im[:,:,2] = new_im_chan
+        #new_im *= 255
+        #new_im = new_im.astype(np.int)
 
         cv.imshow('frame', new_im)
         if cv.waitKey(1) & 0xFF == ord('q'):
