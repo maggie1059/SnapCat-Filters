080b6f797933c1a49224769a70bbfdeb125181a6
maggie1059 <34173352+maggie1059@users.noreply.github.com>
2020-05-01 12:49:23 -0700

    fixed video ashhat

--- video.py
+++ video.py
@@ -2,11 +2,12 @@ import argparse
 import cv2 as cv
 from cv2 import imread
 import numpy as np
-from skimage.color import rgb2gray
+from skimage.color import rgb2gray, gray2rgb
 from skimage.transform import resize
 from model import Model
 import tensorflow as tf
 from add_filter import ashhat_filter
+from add_bow import bow_filter
 
 def preprocess(image):
     # Change to gray scale
@@ -33,20 +34,24 @@ def main():
     model.compile_model()
     model.load_trained_model()
 
-    cap = cv.VideoCapture('live.mp4')
+    cap = cv.VideoCapture(0)
 
     while(cap.isOpened()):
         ret, frame = cap.read()
         img, scaling_factor = preprocess(frame)
-        # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         predicted = model.model.predict(img)
         predicted = tf.reshape(predicted, (9,2))
         predicted = predicted.numpy()
 
         coords = np.floor(predicted * scaling_factor).astype(np.int)
         new_im = ashhat_filter(frame, coords)
-        new_im *= 255
-        new_im = new_im.astype(np.int)
+        new_image_chan = np.copy(new_im[:,:,0])
+        new_im[:,:,0] = new_im[:,:,2]
+        new_im[:,:,2] = new_image_chan
+        # new_im = bow_filter(frame, coords)
+        # new_im *= 255
+        # new_im = new_im.astype(np.int)
 
         cv.imshow('frame', new_im)
         if cv.waitKey(1) & 0xFF == ord('q'):
