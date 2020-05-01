ae366655683adb8c646bb870031a7782a0b7638a
maggie1059 <34173352+maggie1059@users.noreply.github.com>
2020-05-01 12:23:11 -0700

    toast filter

--- add_toast.py
+++ add_toast.py
@@ -25,6 +25,8 @@ def toast_filter(image,coord):
     left_out_ear = coord[3,:]
     right_out_ear = coord[-1,:]
 
+    mouth = coord[2, :]
+
     #try using mouth point for face
     mouth_point = coord[2,:]
     toast = cv2.imread('filters/toast.png',cv2.IMREAD_UNCHANGED)
@@ -33,19 +35,14 @@ def toast_filter(image,coord):
     #set width accordingly
     angle = np.arctan((left_tip[1]-right_tip[1])/(left_tip[0]-right_tip[0]))
     toast_width = np.linalg.norm(right_tip - left_tip)
+    toast_width *= 1.8
     toast_height = mouth_point[1] - min(right_tip[1],left_tip[1])
-    #print("left_tip", left_tip)
-    #print("right_tip", right_tip)
-    #print("mouth_point", mouth_point)
-
-    #print("height", james_height)
-    #print("width",james_width)
+    toast_height *= 2.2
 
     rotate_toast = imutils.rotate_bound(toast,angle*180/np.pi)
 
     old_height, old_width, channel = toast.shape
 
-    #hat_height = old_height*hat_width/old_width
     toast = cv2.resize(rotate_toast, (int(1.3*toast_width),int(1.3*toast_height)))
     hw,hh,hc = toast.shape
 
@@ -63,25 +60,24 @@ def toast_filter(image,coord):
     image[:,:,0] = image[:,:,2]
     image[:,:,2] = new_image_chan
 
+    y = int(mouth[1] - toast_height/1.3)
+    x = int(mouth[0] - toast_width/1.6)
+
     for i in range(0,hw):       # Overlay the filter based on the alpha channel
         for j in range(0,hh):
-            if toast[i,j,-1] != 0 and i+left_tip[1] >=0 and j+left_tip[0] >=0:
-                image[i+left_tip[1],j+left_tip[0],:] = toast[i,j,:-1]
-            '''
-            #use ear tips if out of bounds
-            if left_tip[1] <0 or left_tip[0] <0:
-                if hat[i,j,-1] != 0 and i+left_tip[1] >=0 and j+left_tip[0] >=0:
-                    image[i+left_tip[1],j+left_tip[0],:] = hat[i,j,:-1]
-            #use ear base otherwise
-            else:
-                if hat[i,j,-1] != 0 and i+left_out_ear[1]-hw >=0 and j+left_out_ear[0] >=0:
-                    image[i+left_out_ear[1],j+left_out_ear[0],:] = hat[i,j,:-1]
-            '''
+            if toast[i,j,-1] != 0 and i+y >=0 and j+x >=0 and i+y < len(image) and j+x < len(image[0]):
+                # print(i+left_tip[1], j+left_tip[0])
+                # print(i,j)
+                # image[i+left_tip[1],j+left_tip[0],:] = toast[i,j,:-1]
+                # image[i+left_tip[1]-80,j+left_tip[0]-180,:] = toast[i,j,:-1]
+                # y = j + mouth[0] - toast_height/2
+                # x = i + mouth[1] - toast_width/2
+                image[i+y,j+x,:] = toast[i,j,:-1]
 
     plt.imshow(image)
     plt.show()
 
-data_dir = os.path.dirname(__file__) + './cat-dataset/s'# + str(i)
+data_dir = os.path.dirname(__file__) + './cat-dataset/small'
 image_paths, coordinate_paths = get_cats(data_dir)
 print("retrieved paths")
 images_orig, coords_orig, max_x, max_y = load_data(image_paths, coordinate_paths)
@@ -97,4 +93,4 @@ for ind in range(len(images)):
     print(image.shape)
     coord = coords[ind]
 
-    james_filter(image,coord)
+    toast_filter(image,coord)
